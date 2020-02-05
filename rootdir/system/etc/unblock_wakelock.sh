03e33a93ed122f96ae33ec4f1becd0feb4d5733b
Alfred Neumayer <dev.beidl@gmail.com>
2020-02-05 22:29:56 +0100

    unblock_wakelock: Disable for now

--- rootdir/system/etc/unblock_wakelock.sh
+++ rootdir/system/etc/unblock_wakelock.sh
@@ -1,14 +1,14 @@
 #!/system/bin/sh
 
-wakelock=sns_periodic_wakelock
+#wakelock=sns_periodic_wakelock
 
-for i in 1 2 3 4 5; do
-    sleep 1
-    if grep $wakelock /proc/wakelocks > /dev/null 2>&1; then
-        echo $wakelock > /sys/power/wake_unlock
-        break
-    fi
-done
+#for i in 1 2 3 4 5; do
+#    sleep 1
+#    if grep $wakelock /proc/wakelocks > /dev/null 2>&1; then
+#        echo $wakelock > /sys/power/wake_unlock
+#        break
+#    fi
+#done
 
 # The kernel has problems properly entering suspend
 # so just keep the device up for now.
